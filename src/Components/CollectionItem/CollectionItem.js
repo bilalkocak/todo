fd4b82893daa8fe49a2563fe207335b17b93978a
TURKCELL\\EXT02D23482 <bilal.kocak@consultant.turkcell.com.tr>
2021-02-22 22:53:11 +0300

    Added click feature to collection item

--- src/Components/CollectionItem/CollectionItem.js
+++ src/Components/CollectionItem/CollectionItem.js
@@ -1,15 +1,22 @@
 import React from 'react';
 import {Typography} from 'antd';
 import PropTypes from 'prop-types';
+import {FolderFilled} from "@ant-design/icons";
+import {useHistory} from 'react-router-dom'
+
 
 import './CollectionItem.scss'
 
 const {Title, Text} = Typography;
 
 const CollectionItem = ({collection}) => {
+    const history = useHistory()
+
     return (
-        <div className={'collectionContainer'}>
-            <div className={'collectionIcon'}></div>
+        <div onClick={() => history.push(`/detail/${collection.id}`)} className={'collectionContainer'}>
+            <div className={'collectionIcon'} style={{backgroundColor: collection.color}}>
+                <FolderFilled/>
+            </div>
             <div className={'collectionInfo'}>
                 <Title level={4}>{collection.name}</Title>
                 <Text type="secondary">{collection.desc}</Text>
