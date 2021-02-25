c1803f4f3cc23b517c261f9ce6e5771f89cd048a
Bilal Koçak <kocakbilal04@gmail.com>
2021-02-25 08:19:56 +0300

    updated api url

--- src/api/webApi.js
+++ src/api/webApi.js
@@ -1,51 +1,53 @@
 import axios from "axios";
 
+const BASE_API_URL = 'https://riteg-todo-be.herokuapp.com'
+
 export const fetchTasksApi = (id) => {
-    const url = `https://riteg-todo.herokuapp.com/collections/${id}/todos`;
+    const url = `${BASE_API_URL}/collections/${id}/todos`;
     return axios.get(url)
 }
 
 export const fetchAllTasksApi = () => {
-    const url = `https://riteg-todo.herokuapp.com/todos/`;
+    const url = `${BASE_API_URL}/todos/`;
     return axios.get(url)
 }
 
 export const addTasksApi = (data) => {
-    const url = `https://riteg-todo.herokuapp.com/todos`;
+    const url = `${BASE_API_URL}/todos`;
     return axios.post(url, data)
 }
 
 export const updateTaskApi = (data) => {
-    const url = `https://riteg-todo.herokuapp.com/todos/${data.id}`;
+    const url = `${BASE_API_URL}/todos/${data.id}`;
     return axios.put(url, data)
 }
 
 export const deleteTaskApi = (id) => {
-    const url = `https://riteg-todo.herokuapp.com/todos/${id}`;
+    const url = `${BASE_API_URL}/todos/${id}`;
     return axios.delete(url)
 }
 
 export const fetchCollectionsApi = () => {
-    const url = 'https://riteg-todo.herokuapp.com/collections';
+    const url = `${BASE_API_URL}/collections`;
     return axios.get(url)
 }
 
 export const fetchCollectionByIdApi = (id) => {
-    const url = `https://riteg-todo.herokuapp.com/collections/${id}`;
+    const url = `${BASE_API_URL}/collections/${id}`;
     return axios.get(url)
 }
 
 export const addCollectionsApi = (data) => {
-    const url = 'https://riteg-todo.herokuapp.com/collections';
+    const url = `${BASE_API_URL}/collections`;
     return axios.post(url, data)
 }
 
 export const updateCollectionApi = (data) => {
-    const url = `https://riteg-todo.herokuapp.com/collections/${data.id}`;
+    const url = `${BASE_API_URL}/collections/${data.id}`;
     return axios.put(url, data)
 }
 
 export const deleteCollectionApi = (id) => {
-    const url = `https://riteg-todo.herokuapp.com/collections/${id}`;
+    const url = `${BASE_API_URL}/collections/${id}`;
     return axios.delete(url)
 }
